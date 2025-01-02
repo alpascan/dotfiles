1827c55eaf1a8abc1489d6495691b8e388fca8c1
Alexandru Pascan <alexandru.pascan@nexthink.com>
2025-01-02 20:15:48 +0100

    tmux skin done

--- modules/home-manager/shells/tmux/default.nix
+++ modules/home-manager/shells/tmux/default.nix
@@ -1,56 +1,82 @@
-{ config, pkgs, lib, ... }:{
+{ config, pkgs, lib, ... }: {
   programs.tmux = {
     enable = true;
+    clock24 = true;
+    baseIndex = 1;
+    mouse = true;
+    prefix = "C-q";
+    terminal = "xterm-256color";
+    shell = "${pkgs.nushell}/bin/nu";
+    
+    plugins = with pkgs.tmuxPlugins; [
+      sensible
 
-    # Your custom tmux configuration
+      {
+        plugin = catppuccin;
+        extraConfig = ''
+          set -g @catppuccin_flavour "mocha"
+          
+          # Base colors
+          set -g @catppuccin_status_background "#222223"
+          set -g @catppuccin_status_foreground "#FDF9F3"
+
+          
+          # Window settings
+          set -g @catppuccin_window_left_separator ""
+          set -g @catppuccin_window_right_separator " "
+          set -g @catppuccin_window_middle_separator " █"
+          set -g @catppuccin_window_number_position "right"
+          set -g @catppuccin_window_default_fill "number"
+          set -g @catppuccin_window_default_text "#W"
+          set -g @catppuccin_window_current_fill "number"
+          set -g @catppuccin_window_current_text "#W#{?window_zoomed_flag,(),}"
+
+          # Window colors
+          set -g @catppuccin_window_default_background "#363537"
+          set -g @catppuccin_window_default_foreground "#FDF9F3"
+          set -g @catppuccin_window_current_background "#FC618D"
+          set -g @catppuccin_window_current_foreground "#222223"
+          
+          # Status modules
+          set -g @catppuccin_status_modules_right "user host uptime date_time"          
+          set -g @catppuccin_date_time_text "
+          %d-%m-%Y"
+          
+          # Status colors
+          set -g @catppuccin_directory_color "#7BD88F"
+          set -g @catppuccin_date_time_color "#FD9353"
+          set -g @catppuccin_session_color "#948AE3"
+          set -g @catppuccin_user_color "#FC618D"
+          set -g @catppuccin_host_color "#5AD4E6"
+          set -g @catppuccin_uptime_color "#7BD88F"
+          
+          # Status separators
+          set -g @catppuccin_status_left_separator  " " 
+          set -g @catppuccin_status_right_separator " "        
+          set -g @catppuccin_status_right_separator_inverse "no"
+          set -g @catppuccin_status_fill "icon"
+          set -g @catppuccin_status_connect_separator "no"
+          
+          # Additional highlighting
+          set -g @catppuccin_pane_border_style "fg=#363537"
+          set -g @catppuccin_pane_active_border_style "fg=#FC618D"
+        '';
+      }
+    ];
+
     extraConfig = ''
-      # List of plugins
-      set -g @plugin 'tmux-plugins/tpm'
-      set -g @plugin 'tmux-plugins/tmux-sensible'
-      set -g @plugin 'maxpetretta/tmux-monokai-pro'
-      set -g @plugin 'yardnsm/tmux-1password'
-
-      set -g default-terminal "xterm-256color"
-      set -g base-index 1
-      setw -g pane-base-index 1
-      set -g @monokai-show-empty-plugins false
-      set -g @monokai-show-powerline false
-      set -g @monokai-show-flags true
-      set -g @monokai-kubernetes-hide-user true
-      set -g @monokai-kubernetes-eks-hide-arn true
-      set -g @monokai-show-fahrenheit false
-      set -g @monokai-show-location false
-      set -g @monokai-fixed-location "Lausanne"
-
-      # Reload config
-      bind r source-file ~/.tmux.conf
-
-      # Enable mouse mode
-      set -g mouse on
-      set -g status-right '#(echo $ZSH_VI_MODE)'
-
-      # Window naming and key bindings
-      set-option -g allow-rename on
-      unbind-key C-b
-      set-option -g prefix C-q
-      bind-key C-q send-prefix
+      # Cursor shape integration
+      set -g -a terminal-overrides ',*:Ss=\E[%p1%d q:Se=\E[2 q'
+      
       bind | split-window -h -c "#{pane_current_path}"
       bind - split-window -v -c "#{pane_current_path}"
       bind c new-window -c "#{pane_current_path}"
       unbind '"'
       unbind %
       bind-key q kill-pane
-
-      run '~/.tmux/plugins/tpm/tpm'
+      
+      # Nushell specific settings
+      set -g default-command "${pkgs.nushell}/bin/nu"
     '';
   };
-
-home.activation.installTPM = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-  export PATH=${pkgs.git}/bin:$PATH
-  if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
-    echo "Installing TPM..."
-    git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
-  fi
-'';
-
 }
